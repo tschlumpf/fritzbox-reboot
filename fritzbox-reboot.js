083b78cb8f55e9e93845e775dcc49b0b5e5dcd04
tschlumpf <simon.faessler@mailbox.org>
2021-07-26 21:02:27 +0000

    use a clone of the tr-064 npm package, variable "homedir" added

--- fritzbox-reboot.js
+++ fritzbox-reboot.js
@@ -3,15 +3,16 @@ esversion: 8
 */
 const debug = false;
 
+const homedir = require('os').homedir();
 const connectionTester = require("connection-tester");
-const tr = require("tr-064");
+const tr = require(homedir + "/tr-064");
 const tr064 = new tr.TR064();
 const fs = require("fs");
 
 const fritzConfig = {
     user: "root",
     passwd: "",
-    passwdPath: "/home/simon/fritzboxPasswd",
+    passwdPath: homedir + "/fritzboxPasswd",
     ipAdress: "192.168.178.1",
     minUptime: 600, // seconds
     tr064Port: 49000,
