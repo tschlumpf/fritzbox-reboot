c1c8452d62290dbf1f907023fa32a7549ef983fc
tschlumpf <23003009+tschlumpf@users.noreply.github.com>
2022-07-22 20:20:14 +0200

    reduce dependencies

--- fritzbox-reboot.js
+++ fritzbox-reboot.js
@@ -1,17 +1,17 @@
 /*jshint
 esversion: 8
 */
-const debug = false;
+"use strict";
 
+const debug = true;
 if (debug) {
   log("debug active");
 }
 
 const fs = require("fs");
+const net = require("net");
+const Fritzbox = require("@seydx/fritzbox");
 const homedir = require("os").homedir();
-const connectionTester = require("connection-tester");
-const tr = require("tr-064");
-const tr064 = new tr.TR064();
 
 const fritzConfig = {
   user: "fritz8946",
@@ -20,19 +20,21 @@ const fritzConfig = {
   ipAdress: "192.168.178.1",
   minUptime: 600, // seconds
   tr064Port: 49000,
-  tr064ServiceReboot: "urn:dslforum-org:service:DeviceConfig:1",
-  tr064ServiceUptime: "urn:dslforum-org:service:DeviceInfo:1",
+  tr064ServiceReboot: "urn:DeviceConfig-com:serviceId:DeviceConfig1",
+  tr064ServiceUptime: "urn:DeviceInfo-com:serviceId:DeviceInfo1",
 };
+
 const testConfig = {
   dontTest: false, // If true the FB will allways rebooted.
-  target: "google.com",
+  target: "8.8.8.8",
   port: 443,
   timeout: 3000,
 };
 
+let fritzbox;
+
 run();
 async function run() {
-  "use strict";
   if (!fritzConfig.passwd) {
     // read password from file
     if (!fritzConfig.passwdPath) {
@@ -48,30 +50,28 @@ async function run() {
     return;
   }
 
+  fritzbox = new Fritzbox({ username: fritzConfig.user, password: fritzConfig.passwd, });
+
   if (testConfig.dontTest) {
-    reboot(fritzConfig.passwd, ret => {
-      log(ret);
-    });
+    reboot();
     return;
   }
 
-  connectionTester.test(
+  testConnection(
     testConfig.target,
     testConfig.port,
     testConfig.timeout,
     (err, output) => {
       if (!output.success) {
         // no internet connection. reboot required
-        getUptime(fritzConfig.passwd, uptime => {
+        getUptime(uptime => {
           log("uptime=" + uptime);
           if (uptime === 0) {
             log("uptime invalid. no reboot.");
             return;
           }
           if (uptime > fritzConfig.minUptime) {
-            reboot(fritzConfig.passwd, ret => {
-              log(ret);
-            });
+            reboot();
             return;
           }
           log(`last reboot less then ${fritzConfig.minUptime} seconds ago (${uptime} seconds)`);
@@ -86,63 +86,43 @@ async function run() {
 }
 
 // reboot the FritzBox
-function reboot(passwd, callback) {
-  "use strict";
-  tr064.initTR064Device(fritzConfig.ipAdress, fritzConfig.tr064Port, function (err, device) {
-    if (!err) {
-      device.startEncryptedCommunication(function (err, sslDev) {
-        if (!err) {
-          sslDev.login(fritzConfig.user, passwd);
-          const deviceConfig = sslDev.services[fritzConfig.tr064ServiceReboot];
-          if (!debug) {
-            deviceConfig.actions.Reboot(function (err, result) {
-              // console.log(result);
-              // console.log(err);
-            });
-          }
-          callback("Fritzbox will be rebooted.");
-        }
-      });
+async function reboot() {
+  try {
+    log("Reboot started.");
 
-    } else {
-      callback("no internet connection and no Fritzbox reachable.");
-    }
-  });
+    if (debug) return;
+
+    await fritzbox.exec(
+      fritzConfig.tr064ServiceReboot,
+      "Reboot",
+    );
+
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 // get FritzBox uptime
-async function getUptime(passwd, callback) {
-  "use strict";
-  tr064.initTR064Device(fritzConfig.ipAdress, fritzConfig.tr064Port, function (err, device) {
-    if (!err) {
-      device.startEncryptedCommunication(function (err, sslDev) {
-        if (!err) {
-          sslDev.login(fritzConfig.user, passwd);
-          const deviceUptime = sslDev.services[fritzConfig.tr064ServiceUptime];
-          if (!deviceUptime) {
-            callback(0);
-            return;
-          }
-          deviceUptime.actions.GetInfo(function (err, result) {
-            if (!err) {
-              callback(result.NewUpTime);
-              return;
-            }
-            callback(0);
-          });
-          return;
-        }
-        callback(0);
-      });
-    } else {
-      callback(0);
+async function getUptime(callback) {
+  try {
+    const info = await fritzbox.exec(
+      fritzConfig.tr064ServiceUptime,
+      "GetInfo",
+    );
+
+    if (info.NewUpTime == null) {
+      throw new Error(info);
     }
-  });
+
+    callback(info.NewUpTime);
+  } catch (err) {
+    console.error(err);
+    callback(0);
+  }
 }
 
 // read file
 function readFile(path, callback) {
-  "use strict";
   let output;
 
   try {
@@ -156,7 +136,36 @@ function readFile(path, callback) {
 
 // write log to stdout
 function log(text) {
-  "use strict";
   const timestamp = new Date().toISOString().replace(/-/g, "").replace(/:/g, "").replace("T", "-").split(".")[0];
   console.log(timestamp + "..." + text);
+}
+
+function testConnection(host, port, connectTimeout, callback) {
+  const socket = new net.Socket();
+
+  const output = {
+    success: false,
+    error: null,
+  };
+
+  socket.connect(port, host);
+  socket.setTimeout(connectTimeout);
+
+  socket.on("connect", function () {
+    socket.destroy();
+    output.success = true;
+    return callback(null, output);
+  });
+
+  socket.on("error", function (err) {
+    socket.destroy();
+    output.error = err && err.message || err;
+    return callback(err, output);
+  });
+
+  socket.on("timeout", function (err) {
+    socket.destroy();
+    output.error = err && err.message || err || "socket TIMEOUT";
+    return callback(err, output);
+  });
 }
